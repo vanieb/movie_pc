5d0030ba1d84d6ee3f69d928e8c30fe0aeadf5a6
Vanessa Joy Burgos <vienna@unnotechmanila.com>
2021-05-17 22:09:54 +0800

    Added description

--- src/pages/Bio.vue
+++ src/pages/Bio.vue
@@ -2,16 +2,21 @@
   <v-container fluid>
     <v-row>
       <v-col cols="12" md="4" style="padding:0px;" v-for="i in artists" :key="i.title">
+        <v-container>
         <v-card flat class="rounded-0">
           <v-img
             :src="`${host}${i.image_url}`"
-            class="grey darken-4"
+            class="grey darken-4 rela"
             contain
-          ></v-img>
+          >
+            <div class="abs">{{i.description}}</div>
+          </v-img>
+
           <v-card-title class="title align-center">
             {{i.name}}
           </v-card-title>
         </v-card>
+        </v-container>
       </v-col>
     </v-row>
   </v-container>
@@ -39,4 +44,25 @@ export default {
   }
 };
 </script>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.rela {
+  position: relative;
+  text-align: center;
+  color: white;
+}
+.abs {
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  font-size: 12px;
+  z-index: 999;
+  background: rgba(0,0,0,0.2);
+  text-align: center;
+}
+</style>
